0f22745cecb15a88e4c2353e79200a7bd6c9629f
recreationx <nanzheng03@gmail.com>
2021-02-28 20:02:53 +0800

    Added unstable ICS support

--- README.md
+++ README.md
@@ -14,15 +14,19 @@ pip install -r requirements.txt
 ```
 Run `timetableparse.py`
 ```
-python timetableparse.py [filename] [outputname]
+python timetableparse.py [filename] [outputname] [--type]
 ```
-where `filename` is your HTML filename (`sample.html`, `sample.htm`) and `outputname` is the name of your `outputname.json` file
+where `filename` is your HTML filename (`sample.html`, `sample.htm`) and `outputname` is the name of your `outputname.json` file <br>
+`--type` (`json` or `ics`) will default to JSON if not specified.
 
 ## Note: Your `index_ex.php` is required as your HTML file.
 Save your index_ex.php (timetable page) as a .html/htm from [NYXchange](https://nanyangjc.org/nyapps/timetable/) after logging in.
 
 # Output
-
+## ICS
+A Calendar (.ics) file (universal calendar format) will be generated with the timezone set as `Asia/Singapore` (GMT+8).<br>
+This can be used for importing into various calendar programs (Google Calendar etc.)
+## JSON
 Output file will be a `.json` file, in the following format. Sample is truncated to avoid sensitive information leak. <br>
 `...` signifies that there are more entries. Lessons without a specified venue automatically defaults to `'NIL'`.
 
--- timetableparse.py
+++ timetableparse.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup
+from ics import Calendar, Event
+import pytz
+import datetime
 import json
 import os.path
 import sys
 import argparse
 
+
 parser = argparse.ArgumentParser(description='Process a NYJC timetable.')
 parser.add_argument('filename', help='HTML file, in the same directory')
-parser.add_argument('outputname', help='Output filename (.json)')
+parser.add_argument('outputname', help='Output filename')
+parser.add_argument('--type', help='Output type (ics or json)', default='json')
 args = parser.parse_args()
 
 if args.filename.endswith('.html') is False and args.filename.endswith('.htm') is False:
@@ -67,10 +72,45 @@ for day in days:
                         x += 1
         day_dict[day_name] = day_list
 
-        
-with open(f'{args.outputname}.json', 'w') as outfile:
-    json.dump(day_dict, outfile, indent=4)
-print(day_dict)
+# sorry for the trash variable naming i was tired
+def createcalendar():
+    calendar = Calendar()
+    timezone = pytz.timezone("Asia/Singapore")
+    start_date = str(input("Start date (DD/MM/YYYY): "))
+    start_date = timezone.localize(datetime.datetime.strptime(start_date, "%d/%m/%Y"))
+    current_date = None
+    for c, day in enumerate(day_dict.keys()):
+        schedule = day_dict[day]
+        events = [i for i in schedule if i['name'] != 'No Lesson']
+        events_namelist = [i['name'] for i in events]
+        duration = []
+        final = []
+        for i in events:
+            if any(i['name'] == d['name'] for d in final) is False:
+                final.append({"name": i['name'], "duration": events_namelist.count(i['name']), "begin": i['time'][0:4], 'venue': i['venue']})
+        for i in final:
+            e = Event()
+            e.name = i['name']
+            e.description = i['venue']
+            if c <= 4:
+                current_date = start_date.replace(hour=int(i['begin'][:2]), minute=int(i['begin'][2:4])) + datetime.timedelta(hours=24*c)
+            elif c > 4:
+                current_date = start_date.replace(hour=int(i['begin'][:2]), minute=int(i['begin'][2:4])) + datetime.timedelta(hours=24*(c+2))
+            e.begin = current_date
+            e.end = current_date + datetime.timedelta(minutes=int(i['duration']) * 30)
+            calendar.events.add(e)
+
+    with open(f'{args.outputname}.ics', 'w') as f:
+        f.writelines(calendar)
+
+if args.type == 'json':      
+    with open(f'{args.outputname}.json', 'w') as outfile:
+        json.dump(day_dict, outfile, indent=4)
+elif args.type == 'ics':
+    createcalendar()
+
+
+
 
     
 
